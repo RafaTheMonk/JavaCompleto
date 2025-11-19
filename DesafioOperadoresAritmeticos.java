58b075eea318718ca6b3f95df4d28beaee940d04
Rafael <rafaelbritovaladares@gmail.com>
2025-11-19 17:58:16 -0300

    Atualizando o segundo desafio da seção 3

--- DesafioOperadoresAritmeticos.java
+++ DesafioOperadoresAritmeticos.java
@@ -3,11 +3,21 @@ package seção3;
 public class DesafioOperadoresAritmeticos {
 
 	public static void main(String[] args) {
-		double firstOpe = (Math.pow(6 * (3 + 2), 2)) / (3*2);
-		double secondOpe = Math.pow((1-5) * (2-7) / 2, 2);
-		double thirdOpe =  Math.pow(firstOpe - secondOpe, 3) / Math.pow(10, 3);
+		double numA = Math.pow(6 * (3 + 2), 2);
+		double denA = 3 * 2;
 		
-		System.out.println((int) thirdOpe);
+		double numB = (1-5) * (2-7);
+		double denB = 2;
+		
+		double superiorA = numA / denA;
+		double superiorB = Math.pow(numB / denB, 2);
+		
+		double superior = Math.pow(superiorA - superiorB, 3);
+		double inferior =  Math.pow(10, 3);
+		
+		double resultado = superior / inferior;
+		
+		System.out.println((int) resultado);
 	}
 
 }
